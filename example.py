87d964ce27cef3e32aa9da5834674f80720fccc3
mullerhai <gcya@8342091>
2018-04-19 19:24:10 +0800

    add  example to  operate hive and  generate pandaas dataframe

--- example.py
+++ example.py
@@ -1,7 +1,28 @@
 
 from ftpLogin import client
+from beehive import muHive
 
 if __name__ == '__main__':
+  host = '127.0.0.1'
+  port = 4200
+  username = 'zhuzheng'
+  auth = 'LDAP'
+  password = "abc123."
+  database = 'fkdb'
+  table = 'tab_client_label'
+  paramDict = {'client_nmbr': 'AA75', 'batch': 'p1'}
+
+  filedlist = ['gid', 'realname', 'card']
+  mu = muHive(host, username, password, port)
+  conn = mu.connhive(database, auth)
+  cursor = conn.cursor()
+  limit = 1000
+  df2 = mu.query_selectFileds_Dataframe(conn, table, filedlist, paramDict, 1000)
+  print(df2.shape)
+  print(df2.head())
+  print(df2.columns())
+
+def helloftp():
   host = 'ftps.baidu.com'
   port = '21'
   user = 'zh****eng'
@@ -10,6 +31,7 @@ if __name__ == '__main__':
 
   cli=client(host,user,pwd)
   fs= cli.login(2,True)
+
  # path='haining'
   #cli.ftplistDir(fs,path)
 
